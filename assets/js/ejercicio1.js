ecd0ead20e8cbf8c3b7befd5bb09099e7363b06f
FR4NK080 <francobardiaz@gmail.com>
2023-08-05 00:42:45 -0300

    exercise 2 ready

---
+++ assets/js/ejercicio1.js
@@ -0,0 +1,12 @@
+function borderImg(){
+    document.querySelector(".borde").style.border = "2px solid red"
+    if (onclick === false){
+        document.querySelector(".borde").style.border = "none"
+    }
+}
+
+imagen = document.querySelector(".borde")
+
+imagen.style.border = "2px solid red"
+
+/* imagen.style.border = "none" */
--- assets/js/ejercicio2.js
+++ assets/js/ejercicio2.js
@@ -1,1 +1,18 @@
 
+function compra(){
+    let barco = document.querySelector("#barco").value;
+    let palta = document.querySelector("#palta").value;
+    let oso = document.querySelector("#oso").value;
+    let cantidad = Number(barco) + Number(palta) + Number(oso);
+
+
+    if (cantidad === 0){
+        resultado.innerHTML = "No llevas stickers";
+}
+    else if(cantidad > 10){
+        resultado.innerHTML = "Llevas Demasiados stickers";
+}
+    else{
+        resultado.innerHTML = "Llevas " + cantidad + " stickers";
+}
+}
